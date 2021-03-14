2e504c91edf67b82494db2bd4b98ed7cbe517c50
mitch <jmitchelreed@gmail.com>
2021-03-14 11:26:21 -0400

    added useEffect to solve button double click issue with book results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.scss";
 import axios from "axios";
 import Form from "./Form.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ResultsSection from "./ResultsSection.js";
 import BookChoice from "./BookChoice.js"
 
@@ -34,12 +34,16 @@ function App() {
   const [returnedMovie, setReturnedMovie] = useState();
 
   const handleBookChoice = (clickedButton) => {
-    setBookButtonValue(clickedButton);
-    if (bookButtonValue !== '') {
+    setBookButtonValue(clickedButton); 
+  }
+  // this useEffect will then act once "bookButtonValue" is defined
+  useEffect(()=> {
+
+    if (bookButtonValue) {
+      console.log('if youre seeing this its working properly, OR you clicked twix')
       const matchedBook = returnedBooks.filter((item) => {
         return item.volumeInfo.title === bookButtonValue;
       })
-      // console.log(matchedBook[0].volumeInfo.title);
       setResults([
         {
           type: "movie",
@@ -61,91 +65,99 @@ function App() {
           id: matchedBook[0].volumeInfo.industryIdentifiers[0].identifier,
         },
       ]);
+      
+    } else {
+      // console.log("empty string!!!")
     }
-  }
+  }, [bookButtonValue])
+ 
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSearchMultipleBooks([]);
+    
+    // prevents white space error
+  
 
-    axios({
-      method: "GET",
-      url: `https://api.themoviedb.org/3/search/movie?`,
-      dataResponse: "JSON",
-      params: {
-        format: "JSON",
-        query: userInput,
-        api_key: "6f113bf7cccb0c0c911600f2963a2df4",
-        include_adult: false,
-        language: "en-US",
-      },
-    }).then((response) => {
-      const movieObject = response.data.results[0];
-      // console.log(movieObject);
-      setReturnedMovie(movieObject);
-      // the title that gets sent to the book api
-      const title = response.data.results[0].title;
-
-      // console.log(movieObject)
       axios({
         method: "GET",
-        url: `https://www.googleapis.com/books/v1/volumes?`,
+        url: `https://api.themoviedb.org/3/search/movie?`,
         dataResponse: "JSON",
         params: {
           format: "JSON",
-          q: title,
-          Key: "AIzaSyDDrPYFlXLLrSfJCd7qoXhe1GqUiPj5PQg",
+          query: userInput,
+          api_key: "6f113bf7cccb0c0c911600f2963a2df4",
+          include_adult: false,
+          language: "en-US",
         },
       }).then((response) => {
-        // console.log(response.data.items);
-        const bookObject = response.data.items[0].volumeInfo;
-
-        if (title === bookObject.title) {
-          setResults([
-            {
-              type: "movie",
-              name: movieObject.title,
-              description: movieObject.overview,
-              rating: Math.round(movieObject.vote_average / 2),
-              img: `https://image.tmdb.org/t/p/w200${movieObject.poster_path}`,
-              altDescription: `${movieObject.title} poster`,
-              id: movieObject.id,
-            },
-
-            {
-              type: "book",
-              name: bookObject.title,
-              description: bookObject.description,
-              rating: bookObject.averageRating,
-              img: bookObject.imageLinks.thumbnail,
-              altDescription: `${bookObject.title} cover`,
-              id: bookObject.industryIdentifiers[0].identifier,
-            },
-          ]);
-        } else if (title !== bookObject.title) {
-          axios({
-            method: "GET",
-            url: `https://www.googleapis.com/books/v1/volumes?`,
-            dataResponse: "JSON",
-            params: {
-              format: "JSON",
-              q: title,
-              Key: "AIzaSyDDrPYFlXLLrSfJCd7qoXhe1GqUiPj5PQg",
-            },
-          }).then(response => {
-            const size = 5;
-            const multipleBooks = response.data.items;
-            const newBooksArray = multipleBooks.slice(0, size).map((book) => {
-              return (
-                book.volumeInfo.title
-              )
+        const movieObject = response.data.results[0];
+        // console.log(movieObject);
+        setReturnedMovie(movieObject);
+        // the title that gets sent to the book api
+        const title = response.data.results[0].title;
+  
+        // console.log(movieObject)
+        axios({
+          method: "GET",
+          url: `https://www.googleapis.com/books/v1/volumes?`,
+          dataResponse: "JSON",
+          params: {
+            format: "JSON",
+            q: title,
+            Key: "AIzaSyDDrPYFlXLLrSfJCd7qoXhe1GqUiPj5PQg",
+          },
+        }).then((response) => {
+          // console.log(response.data.items);
+          const bookObject = response.data.items[0].volumeInfo;
+  
+          if (title === bookObject.title) {
+            setResults([
+              {
+                type: "movie",
+                name: movieObject.title,
+                description: movieObject.overview,
+                rating: Math.round(movieObject.vote_average / 2),
+                img: `https://image.tmdb.org/t/p/w200${movieObject.poster_path}`,
+                altDescription: `${movieObject.title} poster`,
+                id: movieObject.id,
+              },
+  
+              {
+                type: "book",
+                name: bookObject.title,
+                description: bookObject.description,
+                rating: bookObject.averageRating,
+                img: bookObject.imageLinks.thumbnail,
+                altDescription: `${bookObject.title} cover`,
+                id: bookObject.industryIdentifiers[0].identifier,
+              },
+            ]);
+          } else if (title !== bookObject.title) {
+            axios({
+              method: "GET",
+              url: `https://www.googleapis.com/books/v1/volumes?`,
+              dataResponse: "JSON",
+              params: {
+                format: "JSON",
+                q: title,
+                Key: "AIzaSyDDrPYFlXLLrSfJCd7qoXhe1GqUiPj5PQg",
+              },
+            }).then(response => {
+              const size = 5;
+              const multipleBooks = response.data.items;
+              const newBooksArray = multipleBooks.slice(0, size).map((book) => {
+                return (
+                  book.volumeInfo.title
+                )
+              })
+              setSearchMultipleBooks(newBooksArray);
+              setReturnedBooks(multipleBooks);
             })
-            setSearchMultipleBooks(newBooksArray);
-            setReturnedBooks(multipleBooks);
-          })
-        }
+          }
+        });
       });
-    });
+    
   };
 
   return (
