640e9351da571b344b51a0435dd3f8e93f86f58d
emma <emmalcgreen@gmail.com>
2021-03-16 21:14:07 -0400

    added react-card flip and resized layout to fit on smaller screen - click for descritpion - if open run npm instal for card-flip library

--- src/ResultContainer.js
+++ src/ResultContainer.js
@@ -1,3 +1,6 @@
+import ReactCardFlip from 'react-card-flip';
+import { useState } from "react";
+
 function ResultContainer(props) {
 	const {
 			name, 
@@ -8,14 +11,30 @@ function ResultContainer(props) {
 			altDescription,
 			outcome
 		} = props;
+
+	const [isFlipped, setIsFlipped] = useState(false);
+
+	function handleCardClick(e) {
+		e.preventDefault();
+		setIsFlipped(!isFlipped);
+	}
+
 		return (
-			<div className={outcome}>
-				<h2 className="mediaTitle">{name}</h2>
-				<p className="author">{author}</p>
-				<div className="imageContainer"><img className="image" src={img} alt={altDescription}/></div>
-				<p className="description">{description}</p>
-				<p className="rating">{rating}</p>
-			</div>
+			<ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal"> 
+					<div className={outcome}>
+						<h2 className="mediaTitle">{name}</h2>
+						<p className="author">{author}</p>
+						<div className="imageContainer"><img className="image" src={img} alt={altDescription}/></div>
+						<button onClick={handleCardClick}>Description</button>
+						<p className="rating">{rating}</p>
+					</div>
+
+					<div className="back">
+						<h2 className="mediaTitle">{name}</h2>
+						<p>{description}</p>
+						<button onClick={handleCardClick}>Go back</button>
+					</div>
+			</ReactCardFlip>
 		)
 }
 
